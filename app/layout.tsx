aad2549c30c297db122dae2e94c0ea18621e38ce
ThanhNguyen5653 <thanhnguyen5653@gmail.com>
2025-05-02 14:01:01 -0700

    Updated metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,35 @@
-import type React from "react"
-import "./globals.css"
-import { Inter } from "next/font/google"
-import { ThemeProvider } from "@/components/theme-provider"
+import type React from "react";
+import "./globals.css";
+import { Inter } from "next/font/google";
+import { ThemeProvider } from "@/components/theme-provider";
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "StreamHub - Live Gaming & IRL Streams",
+  title: "StreamHub - Live Streams",
   description: "Watch your favorite streamers play games and share their lives",
-    generator: 'v0.dev'
-}
+  icons: {
+    icon: [{ url: "/twitch_logo.ico", type: "image/x-icon" }],
+  },
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
           {children}
         </ThemeProvider>
       </body>
     </html>
-  )
+  );
 }
